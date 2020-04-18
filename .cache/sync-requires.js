34b76c3b788e8454ccc0a161e1847decefb3526f
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2020-04-18 11:29:06 +0200

    delete navigation folder

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,6 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/viktorijavalso/are-productions/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/viktorijavalso/are-productions/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/viktorijavalso/are-productions/src/pages/index.js"))),
   "component---src-pages-page-2-js": hot(preferDefault(require("/Users/viktorijavalso/are-productions/src/pages/page-2.js")))
