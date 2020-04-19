3c400abd06162acb4450aa502a7293aaf7eb3c82
Viktorija Valsoe <viktorijavalsoe@gmail.com>
2020-04-19 20:03:28 +0200

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         short_name: `are`,
         start_url: `/`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/images/are-logo.png` // This path is relative to the root of the site.
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
