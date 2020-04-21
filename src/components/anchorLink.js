239b854e886e674f7385d65e4aac496643aed41d
lindastorgard <linda.storgard@gmail.com>
2020-04-21 15:28:42 +0200

    change typo in Instagram and Facebook, anchorlink hover effect

--- src/components/anchorLink.js
+++ src/components/anchorLink.js
@@ -7,22 +7,39 @@ import { ToggleContext } from "./toggleContext";
 const AnchorLink = ({ children, href, isBold }) => {
   const { closeNavigation } = useContext(ToggleContext);
   return (
-    <a
-      aria-label={`Scroll to ${href}`}
-      href={href}
-      onClick={closeNavigation}
-      sx={{
-        fontWeight: isBold ? "heading" : "body",
-        textDecoration: "none",
-        color: "primary",
-        ":hover": {
-          fontWeight: "heading"
-        },
-        ":focus": { fontWeight: "heading" }
-      }}
-    >
-      {children}
-    </a>
+    <div>
+      <a
+        aria-label={`Scroll to ${href}`}
+        href={href}
+        onClick={closeNavigation}
+        sx={{
+          fontWeight: isBold ? "heading" : "body",
+          position: "relative",
+          textDecoration: "none",
+          color: "primary",
+          ":hover": {
+            fontWeight: "heading"
+          },
+          ":focus": { fontWeight: "heading" },
+          ":hover ~ div": {
+            width: "40px",
+            background: "#810904"
+          }
+        }}
+      >
+        {children}
+      </a>
+      <div
+        sx={{
+          width: "0px",
+          height: "2px",
+          background: "#810904",
+          top: "50px",
+          position: "absolute",
+          transition: "0.5s ease"
+        }}
+      ></div>
+    </div>
   );
 };
 AnchorLink.defaultProps = {
