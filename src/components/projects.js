fc51f39402319aa7b215e6b194860617a338445e
Fanny Schriwer <fanny_schriwer@hotmail.com>
2020-04-25 20:21:11 +0200

    add more spacing to side titles for videos

--- src/components/projects.js
+++ src/components/projects.js
@@ -69,7 +69,7 @@ const Projects = () => {
               width: ["100%", "100%", "10%"],
               position: [null, null, "absolute"],
               top: "50%",
-              left: "-60px"
+              left: "-65px"
             }}
           >
             <Styled.h4>{video.node.title}</Styled.h4>
